9a0984998bd08e209d8dda7ce3bee31b50f1e53c
apple <18201497620@163.com>
2020-07-24 12:40:03 +0800

    initCommit

---
+++ app/src/main/java/com/example/unlockninesquares/UnlockNineSquaresView.java
@@ -0,0 +1,22 @@
+package com.example.unlockninesquares;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import androidx.annotation.Nullable;
+
+/**
+ * create by libo
+ * create on 2020/7/24
+ * description
+ */
+public class UnlockNineSquaresView extends View {
+
+    public UnlockNineSquaresView(Context context) {
+        super(context);
+    }
+
+    public UnlockNineSquaresView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
